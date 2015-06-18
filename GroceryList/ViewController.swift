97020b3c43690e7b0d5f451ed01a5ecd4d6c8767
hectorhernandezocampo <USER@MBP-de-USER.lan>
2015-06-18 18:38:01 -0500

    Initial Commit

---
+++ GroceryList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroceryList
+//
+//  Created by USER on 18/06/15.
+//  Copyright (c) 2015 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
